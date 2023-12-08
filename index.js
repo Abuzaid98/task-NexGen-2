32b457cee68d7008d6d021c088ea54b50590618f
Abu Zaid <127389591+Abuzaid98@users.noreply.github.com>
2023-12-08 19:43:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let cards = document.querySelectorAll('.flipCard')
+console.log(cards)
+
+// cards.forEach(card =>{
+//     card.addEventListener('mouseover', ()=>{
+//         console.log(card)
+//     })
+// })
